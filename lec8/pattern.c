7b41b5344a4ddd2971c6773b62e9ed1e72bb2d33
unknown <bhavikakhedkar30@gmail.com>
2025-09-09 19:50:02 +0530

    lec8

---
+++ lec8/pattern.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+
+{
+    int num;
+    for (int i = 0; i < 4; i++)
+    {
+
+        for (int j = 0; j <= i; j++)
+        {
+
+            printf(" ");
+        }
+        for (int k = 3; k > i; k--)
+        {
+            printf("*");
+        }
+
+        for (int p = 3; p >= i; p--)
+        {
+          printf("*");
+        }
+        printf("\n");
+    }
+}
